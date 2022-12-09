ff469217da009e8f75cb72a2f92f71325562bfb0
Bartosz Cylwik <cylwikbartosz@gmail.com>
2022-12-09 15:19:59 +0100

    Zrobiłem eliminację gaussa z II Fazy zadania
    dodatkowo skomentowałem funkcje do wstecznego podstawiania, aby sprawdzic funkcjonalnosc programu poprzez wypisywanie zmienionej maciezy z elem.gaussa i dodałem do comita plik bin/gauss, poniewaz przez uprzednie czynnosci uległ zmianie

--- lmp9/src/main.c
+++ lmp9/src/main.c
@@ -17,9 +17,11 @@ int main(int argc, char ** argv) {
 	printToScreen(b);
 
 	res = eliminate(A,b);
+	printToScreen(A);
+	printToScreen(b);
 	x = createMatrix(b->r, 1);
 	if (x != NULL) {
-		res = backsubst(x,A,b);
+		//res = backsubst(x,A,b);
 
 		printToScreen(x);
 	  freeMatrix(x);
