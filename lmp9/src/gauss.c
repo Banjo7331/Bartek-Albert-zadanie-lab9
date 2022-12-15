a5694832e1eb46b8d84d48af45e444da49f2ae4b
Bartosz Cylwik <cylwikbartosz@gmail.com>
2022-12-15 07:32:05 +0100

    ooo

--- lmp9/src/gauss.c
+++ lmp9/src/gauss.c
@@ -11,7 +11,8 @@ int eliminate(Matrix *mat, Matrix *b){
 	for( int l= 0; l < h-1; l++ ) {
 		for( int w= l+1; w < h; w++ ) {
        if( mat->data[l][l] == 0 )
-          return 1;
+			if( fe)
+			return 1;
        double m = mat->data[w][l] / mat->data[l][l];
        for( int i= l; i < h; i++ )
 				 mat->data[w][i] -= m * mat->data[l][i];
